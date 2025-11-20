15378f1d72445e586c8676896d62a508b97471f0
Zainab Saigol <z.s.saigol@hotmail.com>
2025-11-20 13:34:09 +0000

    seeded database via api

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+json = URI.parse("https://tmdb.lewagon.com/movie/top_rated").read
+movies = JSON.parse(json)
+
+puts "Seeding movies..."
+index = 0
+10.times do
+  Movie.create(
+    title: movies["results"][index]["title"],
+    overview: movies["results"][index]["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movies["results"][index]["poster_path"]}",
+    rating: (movies["results"][index]["vote_average"]).round(1))
+  index += 1
+end
+puts "Created 10 movies!"
+puts "Seeding movies..."
